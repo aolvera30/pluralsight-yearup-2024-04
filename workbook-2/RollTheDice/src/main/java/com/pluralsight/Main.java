867ea9647b5e86900ea73d1c5a9507abfd687e31
angelica <angelicaolvera490@gmail.com>
2024-04-18 15:54:13 -0700

    workbook 2

---
+++ workbook-2/RollTheDice/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Dice dice = new Dice();
+
+        //interger variables
+        int roll1 = 1;
+        int roll2 = 2;
+        
+
+    }
+}
