95c425114e955234096b9a4cc429d25c0abfc3b9
angelica <angelicaolvera490@gmail.com>
2024-04-18 13:32:53 -0700

    workbook 2 loops

---
+++ workbook-2/LoopingExercises/src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class WhileLoop
+{
+    private static int ilove;
+
+    public static void main(String[] args){
+        ilove = 0;
+        while(ilove<5){
+            System.out.println("I love Java");
+            ilove++;
+        }
+    }
+}
