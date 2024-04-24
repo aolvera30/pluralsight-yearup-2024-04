b4d74f78fe13c76b52dc1efa629f4575f84483c0
angelica <angelicaolvera490@gmail.com>
2024-04-24 14:34:12 -0700

    bedtime stories

--- workbook-3/workbook 3 exercises/BedtimeStories/src/main/java/com/pluralsight/Main.java
+++ workbook-3/workbook 3 exercises/BedtimeStories/src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,13 @@
 package com.pluralsight;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        Scanner userInput = new Scanner(System.in);
     }
 }
