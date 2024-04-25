64e63af57e74987168a672c7d57ca72877ad41d6
angelica <angelicaolvera490@gmail.com>
2024-04-25 11:06:47 -0700

    workbook3 exercise

--- workbook-3/workbook 3 exercises/PayrollCalculator/src/main/java/com/pluralsight/Employee.java
+++ workbook-3/workbook 3 exercises/PayrollCalculator/src/main/java/com/pluralsight/Employee.java
@@ -2,4 +2,33 @@ package com.pluralsight;
 
 public class Employee
 {
+    private double id;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    public Employee(double id, String name, double hoursWorked, double payRate)
+    {
+        this.id = id;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+    public double getId() {return id;}
+    public void setId(double id){this.id = id;}
+
+    public String getName() {return name;}
+    public void setName(String name){this.name = name;}
+
+    public double getHoursWorked() {return hoursWorked;}
+    public void setHoursWorked(double hoursWorked){this.hoursWorked = hoursWorked;}
+
+    public double getPayRate() {return payRate;}
+    public void setPayRate(double payRate){this.payRate = payRate;}
+
+    public double getGrossPay() {
+        return hoursWorked * payRate;
+    }
+
 }
--- workbook-3/workbook 3 exercises/PayrollCalculator/src/main/java/com/pluralsight/Main.java
+++ workbook-3/workbook 3 exercises/PayrollCalculator/src/main/java/com/pluralsight/Main.java
@@ -1,9 +1,22 @@
 package com.pluralsight;
 
+import java.io.File;
+
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+
+    }
+
+    public static void loadEmployee()
+    {
+        // 1. create a file with the path to the actual file
+        File file = new File("Employee info file/employees.csv");
+
+        // 2. open the file using a Scanner
+        
+
+
     }
 }
