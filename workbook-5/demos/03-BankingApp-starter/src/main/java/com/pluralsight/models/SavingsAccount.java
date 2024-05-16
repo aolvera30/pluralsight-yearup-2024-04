54fa5927dcd7e7de6b0376f16957066e5a207919
angelica <angelicaolvera490@gmail.com>
2024-05-16 10:10:17 -0700

    workbook 5

---
+++ workbook-5/demos/03-BankingApp-starter/src/main/java/com/pluralsight/models/SavingsAccount.java
@@ -0,0 +1,31 @@
+package com.pluralsight.models;
+
+public class SavingsAccount extends BankAccount
+{
+    private static final double MIN_BALANCE = 25;
+    public SavingsAccount(int accountNumber, String owner, double balance)
+    {
+        super(accountNumber, owner, balance);
+    }
+
+    @Override
+
+    public boolean canWithdraw(double amount)
+    {
+        double newBalance = getBalance() - amount;
+        return newBalance >= MIN_BALANCE;
+    }
+
+    @Override
+    public double withdraw(double amount)
+    {
+        if(canWithdraw(amount))
+        {
+            // call the withdraw() method of the parent
+            return super.withdraw(amount);
+        }
+
+        return getBalance();
+    }
+
+}
